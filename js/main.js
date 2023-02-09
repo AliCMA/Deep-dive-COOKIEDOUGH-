25ca101d0ab12e22965140c997075e9ebdbf55a5
AliCMA <32778@ma-web.nl>
2023-02-09 20:50:35 +0100

    Klaar les 2 video 1

---
+++ js/main.js
@@ -0,0 +1,15 @@
+class Cookie{
+    name = "";
+    constructor(newName){
+        this.name = newName;
+    }
+}
+
+const test = new Cookie("Jeroen");
+console.log(test);
+
+const tes2 = new Cookie("Ali1");
+console.log(test2);
+
+const test3 = new Cookie("Ali2");
+console.log(test3);
