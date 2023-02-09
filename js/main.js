8c3be075a5d3adbb737ea02f9a93aa346f530d36
AliCMA <32778@ma-web.nl>
2023-02-09 21:44:09 +0100

    les 2 video 6

--- js/main.js
+++ js/main.js
@@ -30,8 +30,29 @@ class Score{
         this.score = this.score + 1;
         this.htmlElement.innerText = this.score;
     }
+
+    subtractScore(){
+        this.score = this.score - 100;
+        this.htmlElement.innerText = this.score;
+    }
+}
+
+class Multiplier{
+    factor = 100;
+    htmlElement = undefined;    
+    cookie = undefined;
+
+    constructor(htmlElement){
+        this.htmlElement = htmlElement;
+        this.cookie = cookie;
+        this.htmlElement.onclick = this.onMultiplierClicked;
+    }
+
+    onMultiplierClicked = () => {
+    this.cookie.score.subtractScore();
+    }
 }
 
 const score = new Score(100, "Default Score", document.getElementById("js--score"));
 const cookie = new Cookie("Default Cookie", document.getElementById("js--cookie"), score);
-
+const jeroen = new Multiplier(document.getElementById("js--multiplier"), cookie);
